7dfd9cba5d6ec17ee39f520ff309d54c76c64861
Fanjie Wang <frank@anomalyinnovations.com>
2017-03-02 16:42:59 -0500

    create-a-login-page

--- src/Routes.js
+++ src/Routes.js
@@ -2,12 +2,14 @@ import React from 'react';
 import { Router, Route, IndexRoute } from 'react-router';
 import App from './App';
 import Home from './containers/Home';
+import Login from './containers/Login';
 import NotFound from './containers/NotFound';
 
 export default (props) => (
   <Router {...props}>
     <Route path="/" component={App}>
       <IndexRoute component={Home} />
+      <Route path="login" component={Login} />
       <Route path="*" component={NotFound} />
     </Route>
   </Router>
