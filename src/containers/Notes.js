c0eeb2a58edf8c8f473576a6cbb9578e81e08370
Fanjie Wang <frank@anomalyinnovations.com>
2017-03-02 17:34:23 -0500

    save-changes-to-a-note

--- src/containers/Notes.js
+++ src/containers/Notes.js
@@ -6,7 +6,7 @@ import {
   ControlLabel,
 } from 'react-bootstrap';
 import LoaderButton from '../components/LoaderButton.js';
-import { invokeApig } from '../libs/awsLib.js';
+import { invokeApig, s3Upload } from '../libs/awsLib.js';
 import config from '../config.js';
 import './Notes.css';
 
@@ -61,7 +61,17 @@ class Notes extends Component {
     this.file = event.target.files[0];
   }
 
+  saveNote(note) {
+    return invokeApig({
+      path: `/notes/${this.props.params.id}`,
+      method: 'PUT',
+      body: note,
+    }, this.props.userToken);
+  }
+
   handleSubmit = async (event) => {
+    let uploadedFilename;
+
     event.preventDefault();
 
     if (this.file && this.file.size > config.MAX_ATTACHMENT_SIZE) {
@@ -70,6 +80,24 @@ class Notes extends Component {
     }
 
     this.setState({ isLoading: true });
+
+    try {
+
+      if (this.file) {
+        uploadedFilename = await s3Upload(this.file, this.props.userToken);
+      }
+
+      await this.saveNote({
+        ...this.state.note,
+        content: this.state.content,
+        attachment: uploadedFilename || this.state.note.attachment,
+      });
+      this.props.router.push('/');
+    }
+    catch(e) {
+      alert(e);
+      this.setState({ isLoading: false });
+    }
   }
 
   handleDelete = async (event) => {
