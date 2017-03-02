76de7a09fb12230cb6d4b7ef3b7646d914cf9447
Fanjie Wang <frank@anomalyinnovations.com>
2017-03-02 17:24:48 -0500

    list-all-the-notes

--- src/containers/Home.js
+++ src/containers/Home.js
@@ -1,15 +1,56 @@
 import React, { Component } from 'react';
+import { withRouter } from 'react-router';
+import {
+  PageHeader,
+  ListGroup,
+} from 'react-bootstrap';
 import './Home.css';
 
-export default class Home extends Component {
+class Home extends Component {
+
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      isLoading: false,
+      notes: [],
+    };
+  }
+
+  renderNotesList(notes) {
+    return null;
+  }
+
+  renderLander() {
+    return (
+      <div className="lander">
+        <h1>Scratch</h1>
+        <p>A simple note taking app</p>
+      </div>
+    );
+  }
+
+  renderNotes() {
+    return (
+      <div className="notes">
+        <PageHeader>Your Notes</PageHeader>
+        <ListGroup>
+          { ! this.state.isLoading
+            && this.renderNotesList(this.state.notes) }
+        </ListGroup>
+      </div>
+    );
+  }
+
   render() {
     return (
       <div className="Home">
-        <div className="lander">
-          <h1>Scratch</h1>
-          <p>A simple note taking app</p>
-        </div>
+        { this.props.userToken === null
+          ? this.renderLander()
+          : this.renderNotes() }
       </div>
     );
   }
 }
+
+export default withRouter(Home);
