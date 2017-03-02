ef2c3bf4579581a3b2c8fd81f091bdc9b148ed8d
Fanjie Wang <frank@anomalyinnovations.com>
2017-03-02 16:35:11 -0500

    handle-404s

--- src/Routes.js
+++ src/Routes.js
@@ -2,11 +2,13 @@ import React from 'react';
 import { Router, Route, IndexRoute } from 'react-router';
 import App from './App';
 import Home from './containers/Home';
+import NotFound from './containers/NotFound';
 
 export default (props) => (
   <Router {...props}>
     <Route path="/" component={App}>
       <IndexRoute component={Home} />
+      <Route path="*" component={NotFound} />
     </Route>
   </Router>
 );
