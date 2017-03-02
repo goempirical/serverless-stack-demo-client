e96727948e2c147c155f22653e42b0d5012d4e64
Fanjie Wang <frank@anomalyinnovations.com>
2017-03-02 17:38:19 -0500

    use-the-hoc-in-the-routes

--- src/Routes.js
+++ src/Routes.js
@@ -7,15 +7,17 @@ import Signup from './containers/Signup';
 import NewNote from './containers/NewNote';
 import Notes from './containers/Notes';
 import NotFound from './containers/NotFound';
+import requireAuth from './components/AuthenticatedComponent';
+import requireUnauth from './components/UnauthenticatedComponent';
 
 export default (props) => (
   <Router {...props}>
     <Route path="/" component={App}>
       <IndexRoute component={Home} />
-      <Route path="login" component={Login} />
-      <Route path="signup" component={Signup} />
-      <Route path="notes/new" component={NewNote} />
-      <Route path="notes/:id" component={Notes} />
+      <Route path="login" component={requireUnauth(Login)} />
+      <Route path="signup" component={requireUnauth(Signup)} />
+      <Route path="notes/new" component={requireAuth(NewNote)} />
+      <Route path="notes/:id" component={requireAuth(Notes)} />
       <Route path="*" component={NotFound} />
     </Route>
   </Router>
