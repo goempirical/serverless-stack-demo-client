c43dc6ea977793a2c0990e2357903ef265e8d103
Fanjie Wang <frank@anomalyinnovations.com>
2017-03-02 17:26:39 -0500

    call-the-list-api

--- src/containers/Home.js
+++ src/containers/Home.js
@@ -3,7 +3,9 @@ import { withRouter } from 'react-router';
 import {
   PageHeader,
   ListGroup,
+  ListGroupItem,
 } from 'react-bootstrap';
+import { invokeApig } from '../libs/awsLib.js';
 import './Home.css';
 
 class Home extends Component {
@@ -17,8 +19,50 @@ class Home extends Component {
     };
   }
 
+  async componentWillMount() {
+    if (this.props.userToken === null) {
+      return;
+    }
+
+    this.setState({ isLoading: true });
+
+    try {
+      const results = await this.notes();
+      this.setState({ notes: results });
+    }
+    catch(e) {
+      alert(e);
+    }
+
+    this.setState({ isLoading: false });
+  }
+
+  onNoteClick = (event) => {
+    event.preventDefault();
+    this.props.router.push(event.currentTarget.getAttribute('href'));
+  }
+
+  notes() {
+    return invokeApig({ path: '/notes' }, this.props.userToken);
+  }
+
   renderNotesList(notes) {
-    return null;
+    return [{}].concat(notes).map((note, i) => (
+      i !== 0
+        ? ( <ListGroupItem
+              key={note.noteId}
+              href={`/notes/${note.noteId}`}
+              onClick={this.onNoteClick}
+              header={note.content.trim().split('\n')[0]}>
+                { "Created: " + (new Date(note.createdAt)).toLocaleString() }
+            </ListGroupItem> )
+        : ( <ListGroupItem
+              key="new"
+              href="/notes/new"
+              onClick={this.onNoteClick}>
+                <h4><b>&#65291;</b> Create a new note</h4>
+            </ListGroupItem> )
+    ));
   }
 
   renderLander() {
